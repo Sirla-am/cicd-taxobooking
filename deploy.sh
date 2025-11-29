501508a511d1596b237c1f3d20a56a3e903b1ff7
sairamguthula14 <sairamguthula14@gmail.com>
2025-11-29 17:03:37 +0530

    node

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-kubectl apply -f namespace.yaml 
-kubectl apply -f deployment.yaml 
-kubectl apply -f secrets.yaml 
-kubectl apply -f service.yaml
+#kubectl apply -f namespace.yaml 
+#kubectl apply -f deployment.yaml 
+#kubectl apply -f secrets.yaml 
+#kubectl apply -f service.yaml
+
+kubectl delete -f namespace.yaml 
+kubectl delete -f deployment.yaml 
+kubectl delete -f secrets.yaml 
+kubectl detele -f service.yaml
